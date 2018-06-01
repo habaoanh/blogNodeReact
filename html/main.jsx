9f61703245b0ca4209baf5febc5ac142ee7fbc54
habaoanh <habaoanh@gmail.com>
2018-06-01 10:01:43 +0800

    Implementing User Sign-In

--- html/main.jsx
+++ html/main.jsx
@@ -1,4 +1,27 @@
 class Signin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  signIn() {
+    alert(
+      "Email address is " +
+        this.state.email +
+        " Password is " +
+        this.state.password
+    );
+  }
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+  }
   render() {
     return (
       <form className="form-signin">
@@ -9,6 +32,7 @@ class Signin extends React.Component {
         </label>
         <input
           type="email"
+          onChange={this.handleEmailChange}
           id="inputEmail"
           className="form-control"
           placeholder="Email address"
@@ -21,12 +45,17 @@ class Signin extends React.Component {
         </label>
         <input
           type="password"
+          onChange={this.handlePasswordChange}
           id="inputPassword"
           className="form-control"
           placeholder="Password"
           required
         />
-        <button className="btn btn-lg btn-primary btn-block" type="button">
+        <button
+          className="btn btn-lg btn-primary btn-block"
+          onClick={this.signIn}
+          type="button"
+        >
           {" "}
           Sign in
         </button>
