d0d43172c75b5a99c9c432fa582f122d28f38487
habaoanh <habaoanh@gmail.com>
2018-06-01 10:21:56 +0800

    Implementing User Sign-In 2

--- html/main.jsx
+++ html/main.jsx
@@ -1,6 +1,7 @@
 class Signin extends React.Component {
   constructor(props) {
     super(props);
+    this.signIn = this.signIn.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.state = {
